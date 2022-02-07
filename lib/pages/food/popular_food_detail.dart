34cb1822d73f02bc28d3ac786a94d1e1bdb5cfd3
Ssekuwanda <cytixdoug@gmail.com>
2022-02-07 07:25:22 +0300

    Popular food screen

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_app/utils/colors.dart';
 import 'package:flutter_food_delivery_app/utils/dimensions.dart';
+import 'package:flutter_food_delivery_app/widgets/app_icon.dart';
+import 'package:flutter_food_delivery_app/widgets/big_text.dart';
+import 'package:flutter_food_delivery_app/widgets/icon_and_text.dart';
+import 'package:flutter_food_delivery_app/widgets/small_text.dart';
 
 class PopularFoodDetail extends StatelessWidget {
   const PopularFoodDetail({Key? key}) : super(key: key);
@@ -24,11 +29,94 @@ class PopularFoodDetail extends StatelessWidget {
             ),
           ),
           Positioned(
-              left: Dimensions.width20,
-              right: Dimensions.width20,
-              child: Row(
-                children: [],
-              ))
+            top: Dimensions.height45,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(icon: Icons.arrow_back_ios),
+                AppIcon(icon: Icons.shopping_cart_outlined),
+              ],
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: Dimensions.popularFoodImgSize - 20,
+            child: Container(
+              padding: EdgeInsets.only(
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(Dimensions.radius20),
+                  topLeft: Radius.circular(Dimensions.radius20),
+                ),
+                color: Colors.white,
+              ),
+              child: Container(
+                padding: EdgeInsets.only(
+                  top: Dimensions.height15,
+                  left: Dimensions.width15,
+                  right: Dimensions.width15,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Chinese Side"),
+                    SizedBox(height: Dimensions.height10),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(
+                            5,
+                            (index) => Icon(
+                              Icons.star,
+                              color: AppColors.mainColor,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        SmallText(text: "4.5"),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SmallText(text: "1287 comments"),
+                      ],
+                    ),
+                    SizedBox(
+                      height: Dimensions.height10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconAndTextWidget(
+                          icon: Icons.circle,
+                          text: "Normal",
+                          iconColor: AppColors.iconColor1,
+                        ),
+                        IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: "1.7km",
+                          iconColor: AppColors.mainColor,
+                        ),
+                        IconAndTextWidget(
+                          icon: Icons.access_time_rounded,
+                          text: "32min",
+                          iconColor: AppColors.iconColor2,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
 
 class Dimensions {
+  // Defualt sreen size is 844
+  // hence 844/20
   static double screenHeight = Get.context!.height;
   static double screenWidth = Get.context!.width;
 
@@ -11,6 +13,7 @@ class Dimensions {
   static double height10 = screenHeight / 84.4;
   static double height20 = screenHeight / 42.2;
   static double height15 = screenHeight / 56.27;
+  static double height45 = screenHeight / 18.8;
 
   static double width10 = screenHeight / 84.4;
   static double width20 = screenHeight / 42.2;
@@ -24,6 +27,7 @@ class Dimensions {
   static double radius30 = screenHeight / 28.13;
 
   static double iconSize24 = screenHeight / 35.17;
+  static double iconSize16 = screenHeight / 52.75;
 
   static double listViewImg = screenWidth / 3.25;
   static double listViewTextContSize = screenWidth / 3.9;
